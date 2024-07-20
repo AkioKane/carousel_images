10ded326eb8578a381fb2263b825bab60dad80e5
Akio <aakocherga@mail.ru>
2024-07-20 20:24:03 +0700

    Added swipe images

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 function btnClick() {
   const btnActive = document.querySelector(".btn-start")
   const btnSwipe = document.querySelector(".btn-swipe")
+  const images1 = document.querySelector(".images")
+  const images2 = document.querySelector(".images-2")
 
   btnActive.addEventListener("click", () => {
     if (btnActive.classList.contains("btn-start")) {
@@ -19,10 +21,44 @@ function btnClick() {
       imageShow("none")
     }
   })
+
+  btnSwipe.addEventListener("click", () => {
+    if (images1.classList.contains("active")) {
+      btnActive.style.display = "none"
+      setTimeout(() => {
+        btnActive.style.display = "flex"
+      }, 0)
+
+      images1.classList.remove("active")
+      images2.classList.add("active")
+      images1.style.display = "none"
+      imageShow("flex")
+
+      btnSwipe.style.display = "none"
+      setTimeout(() => {
+        btnSwipe.style.display = "flex"
+      }, 0)
+    } else if (images2.classList.contains("active")) {
+      btnActive.style.display = "none"
+      setTimeout(() => {
+        btnActive.style.display = "flex"
+      }, 0)
+
+      images2.classList.remove("active")
+      images1.classList.add("active")
+      images2.style.display = "none"
+      imageShow("flex")
+
+      btnSwipe.style.display = "none"
+      setTimeout(() => {
+        btnSwipe.style.display = "flex"
+      }, 0)
+    }
+  })
 }
 
 function imageShow(block) {
-  const images = document.querySelector(".images")
+  const images = document.querySelector(".active")
   const btnActive = document.querySelector(".btn-active")
   if (block === "none") {
     images.classList.add("close")
